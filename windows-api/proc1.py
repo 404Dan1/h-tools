b8f11d76cc7cbab99ca0852d50376e2c8e8faa4f
404Dan <94033250+404Dan1@users.noreply.github.com>
2022-04-19 10:07:59 -0700

    Update proc1.py

--- windows-api/proc1.py
+++ windows-api/proc1.py
@@ -1,1 +1,23 @@
 
+import ctypes
+
+k_handle = ctypes.WinDLL("Kernel32.dll")
+
+PROCESS_ALL_ACCESS = (0x00F0000 | 0x00100000 | 0xFFF)
+
+dWDesiredAccess = PROCESS_ALL_ACCESS
+bInheritHandle = False
+dWProcessId = 0x159c  # process ID in hex
+
+response = k_handle.OpenProcess(dWDesiredAccess, bInheritHandle, dWProcessId)
+
+print(response)
+error = k_handle.GetLastError()
+if error != 0:
+    print(f"Error Code: {error}")
+    exit(1)
+
+if response <= 0:
+    print("The handle was not created")
+else:
+    print("handle was create")
